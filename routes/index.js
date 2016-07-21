41a1278a8cbd28e507ef58814e7e5adfa1c94a08
Alex.Dai <re757575@gmail.com>
2016-07-21 14:07:15 +0000

    初步完成

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var request = require('request');
 var router = express.Router();
 
 /* GET home page. */
@@ -6,4 +7,32 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.route('/services/:param1/:param2/:param3')
+    .post(function(req, res) {
+        console.log(req.params);
+        
+        var salckUrl = 'https://hooks.slack.com/services/'+ req.params.param1 +'/'
+            + req.params.param2 +'/'+ req.params.param3;
+
+        var docker = req.body;
+        
+        console.log(docker);
+
+            var slackText = '[<'+ docker['repository']['repo_url'] +'|'+ docker['repository']['repo_name'] +'>]'+
+                ' new image build complete,'+
+                ' pushed by '+ docker['push_data']['pusher'];
+
+            request({
+                uri: salckUrl,
+                method: 'POST',
+                json: {
+                    'text': slackText
+                }
+            }, function(error, response, result) {
+                console.log(result);
+                res.send(result);
+            });
+    });
+    
+    
 module.exports = router;
